8c003d66c4bb6d8aff40715ecce2cce759748df7
FrankTobback <franktobback@gmail.com>
2019-02-11 10:00:43 +0100

    commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let pokemonName;
+$(document).keydown(function (e) {
+    if (e.keyCode === 13) {
+        pokemonName = $('#pokemonName').val();
+        $.get('https://pokeapi.co/api/v2/pokemon/' + pokemonName, function (data) {
+            $('#number').text("id :" + data.id);
+            $('#photo').attr('src', data.sprites.front_default);
+            $('#photo2').attr('src', data.sprites.back_default);
+            $('#photo3').attr('src', data.sprites.front_shiny);
+            $('#photo4').attr('src', data.sprites.back_shiny);
+            for (i = 0; i < 4; i++) {
+                $($("li")[i]).text(data.moves[i].move.name);
+                $.get('https://pokeapi.co/api/v2/pokemon-species/' + data.id, function (data2) {
+                    let vorige = data2.evolves_from_species;
+                    if (vorige == null) {
+                        $('#evolved').text('Evolved from: no one');
+                        $('#photo5').attr('src', '');
+                    } else {
+                        $('#evolved').html('Evolved from: ' + vorige.name);
+                        $.get('https://pokeapi.co/api/v2/pokemon/' + vorige.name, function (vorigefoto) {
+
+                            $('#photo5').attr('src', vorigefoto.sprites.front_default);
+                        });
+                      }
+                  });
+               }
+           })
+       }
+   });
